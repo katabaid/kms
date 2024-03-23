a699ca013a94d8d13ce98ced1566ac76a38e1dc8
auliabismar <auliabismar@yahoo.com>
2024-03-23 12:16:56 +0700

    KPI, APIs, Leaves

--- kms/api.py
+++ kms/api.py
@@ -35,7 +35,7 @@ def get_item_codes_for_bundle():
   return items
 
 @frappe.whitelist()
-def create_product_bundle_from_quotation(items, name, price_list, party_name, quotation_to):
+def create_product_bundle_from_quotation(items, name, price_list, party_name, quotation_to, price, margin):
   if isinstance(items, str):
     items = json.loads(items)
   item_name = ''
@@ -83,8 +83,8 @@ def create_product_bundle_from_quotation(items, name, price_list, party_name, qu
     
     total_rate = total_rate + rate
     pb_doc.append("items", i)
-  pb_doc.custom_rate = total_rate
-  pb_doc.custom_margin = 0
+  pb_doc.custom_rate = price
+  pb_doc.custom_margin = margin
   pb_doc.custom_enable = 0
   pb_doc.insert();
   message = frappe.get_doc('Product Bundle', pb_doc.name)
@@ -95,7 +95,7 @@ def get_quotation_item(quotation_no):
   items = frappe.db.sql(f"""select
 	tqi.idx,
 	tig.name item_group,
-	concat(tqi.description, ": ", tpb.name) bundle_name,
+	concat(tqi.item_name, ": ", tpb.name) bundle_name,
 	tpb.name,
 	tpbi.item_code,
 	concat(tpbi.description, ": ", tpbi.item_code) item_name,
@@ -204,7 +204,8 @@ def get_items_to_create_bundle():
       (select SUBSTRING_INDEX(concat(concat_ws('|', (select parent_item_group from `tabItem Group` where name = tig.parent_item_group and parent_item_group not in ('All Item Groups', 'Examination')), parent_item_group, name), '|'), '|', 1) from `tabItem Group` tig where name = ti.item_group) as lv1,
       (select SUBSTRING_INDEX(SUBSTRING_INDEX(concat(concat_ws('|', (select parent_item_group from `tabItem Group` where name = tig.parent_item_group and parent_item_group not in ('All Item Groups', 'Examination')), parent_item_group, name), '|'), '|', 2), '|', -1) from `tabItem Group` tig where name = ti.item_group) as lv2,
       (select SUBSTRING_INDEX(SUBSTRING_INDEX(concat(concat_ws('|', (select parent_item_group from `tabItem Group` where name = tig.parent_item_group and parent_item_group not in ('All Item Groups', 'Examination')), parent_item_group, name), '|'), '|', 3), '|', -1) from `tabItem Group` tig where name = ti.item_group) as lv3,
-      name, item_name
+      name, item_name,
+      (select price_list_rate from `tabItem Price` tip where tip.price_list = 'Standard Selling' and tip.item_code = ti.item_code and tip.valid_from < curdate() order by tip.valid_from desc limit 1) as rate
     from tabItem as ti
     where ti.custom_product_bundle = 1 and ti.disabled = 0
     order by 1, 2, 3, 4""", as_dict=True)
@@ -265,13 +266,77 @@ def get_items_to_create_bundles():
     order by 1, 2, 4""", as_dict=True)
   return items
 
+import re
+
+def extract_age_from_string(age_string):
+    age_pattern = re.compile(r'(\d+) Years?')
+    match = age_pattern.match(age_string)
+    if match:
+        return int(match.group(1))
+    else:
+        return None
+    
 @frappe.whitelist()
-def create_sample_and_test_from_encounter(values):
-  samples = frappe.db.sql(f"""select sample, sum(sample_qty) qty from `tabLab Test Template` tltt where name in (select lab_test_code from `tabLab Prescription` where parent = '{encounter}' and parenttype = 'Patient Encounter') group by 1""", as_dict=True)
-  enc_doc = frappe.get_doc({
-    'doctype': 'Patient Encounter',
-    'name': encounter
+def create_sample_and_test_from_encounter(enc, selected):
+  #if isinstance(selected, str):
+  trtrt = json.loads(selected)
+  params = ','.join([f"'{s}'" for s in trtrt])
+  samples = frappe.db.sql(f"""select sample, sum(sample_qty) qty from `tabLab Test Template` tltt where name in ({params}) group by 1""", as_dict=True)
+  enc_doc = frappe.get_doc('Patient Encounter', enc)
+  lab = frappe.db.get_value('Branch', enc_doc.custom_branch, 'custom_default_lab')
+
+  lab_doc = frappe.get_doc({
+    'doctype': 'Lab Test',
+    'custom_appointment': enc_doc.appointment,
+    'patient': enc_doc.patient,
+    'patient_name': enc_doc.patient_name,
+    'patient_age': enc_doc.patient_age,
+    'patient_sex': enc_doc.patient_sex,
+    'company': enc_doc.company,
+    'custom_branch': enc_doc.custom_branch,
+    'custom_service_unit': lab,
+    'normal_toggle': 1,
+    'practitioner': enc_doc.practitioner
   })
+  for test in params.replace("'", "").split(','):
+    age = extract_age_from_string(enc_doc.patient_age)
+    minmax = frappe.db.sql(f"""
+      WITH cte AS (
+        SELECT
+          parent,
+          lab_test_event,
+          custom_age,
+          custom_sex,
+          custom_min_value,
+          custom_max_value,
+          MAX(CASE WHEN custom_age <= {age} THEN custom_age END) OVER (PARTITION BY parent, lab_test_event, custom_sex ORDER BY custom_age DESC) AS max_age
+        FROM
+          `tabNormal Test Template`
+      )
+      SELECT
+        lab_test_event,
+        COALESCE(
+          (SELECT custom_min_value FROM cte WHERE parent = '{test}' AND lab_test_event = c.lab_test_event AND custom_sex = '{enc_doc.patient_sex}' AND max_age = {age} order by custom_age desc limit 1),
+          (SELECT custom_min_value FROM cte WHERE parent = '{test}' AND lab_test_event = c.lab_test_event AND custom_sex = '{enc_doc.patient_sex}' AND custom_age = (SELECT MAX(max_age) FROM cte WHERE parent = '{test}' AND lab_test_event = c.lab_test_event AND custom_sex = '{enc_doc.patient_sex}' AND max_age < {age}))
+        ) AS custom_min_value,
+        COALESCE(
+          (SELECT custom_max_value FROM cte WHERE parent = '{test}' AND lab_test_event = c.lab_test_event AND custom_sex = '{enc_doc.patient_sex}' AND max_age = {age} order by custom_age desc limit 1),
+          (SELECT custom_max_value FROM cte WHERE parent = '{test}' AND lab_test_event = c.lab_test_event AND custom_sex = '{enc_doc.patient_sex}' AND custom_age = (SELECT MAX(max_age) FROM cte WHERE parent = '{test}' AND lab_test_event = c.lab_test_event AND custom_sex = '{enc_doc.patient_sex}' AND max_age < {age}))
+        ) AS custom_max_value
+      FROM
+        cte c
+      WHERE
+        parent = '{test}'
+        AND custom_sex = '{enc_doc.patient_sex}'
+      GROUP BY
+        lab_test_event;
+    """, as_dict=True)
+    min_val = 0
+    max_val = 0
+    for mm in minmax:
+      lab_doc.append('normal_test_items', {'lab_test_name': test, 'custom_min_value': mm.custom_min_value, 'custom_max_value': mm.custom_max_value, 'lab_test_event': mm.lab_test_event})
+  lab_doc.insert()
+
   sample_doc = frappe.get_doc({
     'doctype': 'Sample Collection',
     'custom_appointment': enc_doc.appointment,
@@ -281,20 +346,20 @@ def create_sample_and_test_from_encounter(values):
     'patient_sex': enc_doc.patient_sex,
     'company': enc_doc.company,
     'custom_branch': enc_doc.custom_branch,
-    'custom_service_unit': '',
-
+    'custom_service_unit': lab,
+    'custom_lab_test': lab_doc.name
   })
+  
   for smpl in samples:
     sample_doc.append('custom_sample_table', {'sample': smpl.sample, 'quantity': smpl.qty})
+  print(str(sample_doc))
   sample_doc.insert()
 
-  """lab_doc = frappe.get_doc({
-    'doctitle': 'Lab Test'
-  })
-  lab_doc.insert()
+  for item in params.replace("'", "").split(','):
+    enc_doc.append ("lab_test_prescription", {'lab_test_code':item, 'lab_test_name': item, 'custom_sample_collection': sample_doc.name, 'custom_lab_test': lab_doc.name})
+  enc_doc.save()
 
-  message = {'sample': sample_doc.name, 'lab': lab_doc.name}"""
-  message = {'sample': sample_doc.name}
+  message = {'sample': sample_doc.name, 'lab': lab_doc.name}
   return message
 
 @frappe.whitelist()
@@ -312,3 +377,101 @@ def get_items_to_create_lab():
     and ltt.disabled = 0
     order by 1, 2, 3, 4) a WHERE a.lv1 = 'Laboratory'""", as_dict=True)
   return items
+
+@frappe.whitelist()
+def create_mr_from_encounter(enc):
+  #frappe.throw(enc_doc)
+  encdoc = frappe.get_doc('Patient Encounter', enc)
+  
+  mr_internal_items = frappe.db.sql(f"""
+  select drug_code, drug_name, dosage, period, dosage_form, custom_compound_qty
+  from `tabDrug Prescription`
+  where parent = '{encdoc.name}' and parentfield = 'drug_prescription' and parenttype = 'Patient Encounter' and custom_material_request is null and custom_is_internal = 1 and docstatus = 0
+  """, as_dict=True)
+  mr_external_items = frappe.db.sql(f"""
+  select drug_code, drug_name, dosage, period, dosage_form, custom_compound_qty
+  from `tabDrug Prescription`
+  where parent = '{encdoc.name}' and parentfield = 'drug_prescription' and parenttype = 'Patient Encounter' and custom_material_request is null and custom_is_internal = 0 and docstatus = 0
+  """, as_dict=True)
+  pharmacy_warehouse, front_office = frappe.db.get_value('Branch', encdoc.custom_branch, ['custom_default_pharmacy_warehouse', 'custom_default_front_office'])
+  message =[pharmacy_warehouse, front_office]
+  if(mr_internal_items):
+    warehouse = frappe.db.get_value('Healthcare Service Unit', encdoc.custom_service_unit, 'warehouse')
+    mr_in_doc = frappe.new_doc('Material Request')
+    mr_in_doc.transaction_date = frappe.utils.today()
+    mr_in_doc.material_request_type= 'Medication Prescription'
+    mr_in_doc.schedule_date = frappe.utils.today()
+    mr_in_doc.set_from_warehouse = pharmacy_warehouse
+    mr_in_doc.set_warehouse = warehouse
+    mr_in_doc.custom_appointment = encdoc.appointment,
+    mr_in_doc.custom_patient = encdoc.patient
+    mr_in_doc.custom_patient_name = encdoc.patient_name
+    mr_in_doc.custom_patient_sex = encdoc.patient_sex
+    mr_in_doc.custom_patient_age = encdoc.patient_age
+    mr_in_doc.custom_patient_encounter = encdoc.name
+    mr_in_doc.custom_healthcare_practitioner = encdoc.practitioner
+    for item in mr_external_items:
+      stock_uom = frappe.db.get_value('Item', item.drug_code, 'stock_uom')
+      mr_in_doc.append('items',{
+        'item_code': item.drug_code,
+        'item_name': item.drug_name,
+        'schedule_date': frappe.utils.today(),
+        'description': item.drug_name,
+        'qty': item.custom_compound_qty,
+        'uom': stock_uom,
+        'stock_uom': stock_uom, 
+        'conversion_factor': 1,
+        'from_warehouse': pharmacy_warehouse,
+        'warehouse': warehouse,
+        'custom_dosage': item.dosage,
+        'custom_period': item.period,
+        'custom_dosage_form': item.dosage_form
+      })
+    mr_in_doc.insert(ignore_permissions=True);
+    frappe.db.sql(f"""update `tabDrug Prescription` set custom_material_request = '{mr_in_doc.name}' where parent = '{encdoc.name}' and parentfield = 'drug_prescription' and parenttype = 'Patient Encounter' and custom_material_request is null and custom_is_internal = 1 and docstatus = 0""")
+    message.append(mr_in_doc.name)
+  else: 
+    message.append('Empty internal')
+
+  if(mr_external_items):
+    mr_ex_doc = frappe.new_doc('Material Request')
+    mr_ex_doc.transaction_date = frappe.utils.today()
+    mr_ex_doc.material_request_type= 'Medication Prescription'
+    mr_ex_doc.schedule_date = frappe.utils.today()
+    mr_ex_doc.set_from_warehouse = pharmacy_warehouse
+    mr_ex_doc.set_warehouse = front_office
+    mr_ex_doc.custom_appointment = encdoc.appointment,
+    mr_ex_doc.custom_patient = encdoc.patient
+    mr_ex_doc.custom_patient_name = encdoc.patient_name
+    mr_ex_doc.custom_patient_sex = encdoc.patient_sex
+    mr_ex_doc.custom_patient_age = encdoc.patient_age
+    mr_ex_doc.custom_patient_encounter = encdoc.name
+    mr_ex_doc.custom_healthcare_practitioner = encdoc.practitioner
+    for item in mr_external_items:
+      stock_uom = frappe.db.get_value('Item', item.drug_code, 'stock_uom')
+      mr_ex_doc.append('items',{
+        'item_code': item.drug_code,
+        'item_name': item.drug_name,
+        'schedule_date': frappe.utils.today(),
+        'description': item.drug_name,
+        'qty': item.custom_compound_qty,
+        'uom': stock_uom,
+        'stock_uom': stock_uom, 
+        'conversion_factor': 1,
+        'from_warehouse': pharmacy_warehouse,
+        'warehouse': front_office,
+        'custom_dosage': item.dosage,
+        'custom_period': item.period,
+        'custom_dosage_form': item.dosage_form
+      })
+    mr_ex_doc.insert(ignore_permissions=True);
+    frappe.db.sql(f"""update `tabDrug Prescription` set custom_material_request = '{mr_ex_doc.name}' where parent = '{encdoc.name}' and parentfield = 'drug_prescription' and parenttype = 'Patient Encounter' and custom_material_request is null and custom_is_internal = 0 and docstatus = 0""")
+    message.append(mr_ex_doc.name)
+  else: 
+    message.append('Empty external')
+  return message
+
+@frappe.whitelist()
+def create_attendance_from_checkin(from_date, to_date):
+  
+  pass
---
+++ kms/kms/doctype/kpi_template/kpi_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, GIS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('KPI Template', {
+	// refresh: function(frm) {
+
+	// }
+});
