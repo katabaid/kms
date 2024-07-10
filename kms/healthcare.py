0bdf4b7939bd1df26b67bd321a07b5a13da674bd
auliabismar <auliabismar@yahoo.com>
2024-07-10 12:15:37 +0700

    doctor dispatcher

--- kms/healthcare.py
+++ kms/healthcare.py
@@ -5,6 +5,8 @@ from frappe.utils import today
 def create_service(target, source, name, room):
   if target == 'Nurse Examination':
     result = create_nurse_exam(source, name, room)
+  elif target == 'Doctor Examination':
+    result = create_doctor_exam(source, name, room)
   elif target == 'Radiology':
     result = create_radiology_exam(source, name, room)
   else:
@@ -61,6 +63,54 @@ def create_nurse_exam(source, name, room):
       frappe.throw("No Template found.")
   else:
     frappe.throw(f"Unsupported source: {source}")
+def create_doctor_exam(source, name, room):
+  if source == 'Dispatcher':
+    appt = frappe.db.get_value('Dispatcher', name, 'patient_appointment')
+    appt_doc = frappe.get_doc('Patient Appointment', appt) 
+    doctor_doc = frappe.get_doc({
+      'doctype': 'Doctor Examination',
+      'appointment': appt_doc.name,
+      'patient': appt_doc.patient,
+      'patient_name': appt_doc.patient_name,
+      'company': appt_doc.company,
+      'branch': appt_doc.custom_branch,
+      'dispatcher': name,
+      'service_unit': room,
+      'created_date': today(),
+      'expected_result_date': today(),
+      'status': 'Started'
+    })
+    exam_items = frappe.db.sql(f"""SELECT tnet.name, tnet.item_code 
+      from `tabMCU Appointment` tma, `tabItem Group Service Unit` tigsu, `tabDoctor Examination Template` tnet
+      where tma.parenttype = 'Dispatcher'
+      and tma.parentfield = 'package'
+      and tma.parent = '{name}'
+      AND tnet.item_code = tma.examination_item
+      and tigsu.parenttype = 'Item Group'
+      and tigsu.parentfield = 'custom_units'
+      and tigsu.parent = tma.item_group
+      and tigsu.branch = '{appt_doc.custom_branch}'
+      and tigsu.service_unit = '{room}'""", as_dict=True)
+    if exam_items:
+      for exam_item in exam_items:
+        doctor_doc.append('examination_item', {'template': exam_item.name})
+        net_doc = frappe.get_doc('Doctor Examination Template', exam_item.name)
+
+        selectives = net_doc.get('items')
+        if selectives:
+          for selective in selectives:
+            doctor_doc.append('result', {'result_line': selective.result_text, 'normal_value': selective.normal_value, 'result_check': selective.normal_value, 'item_code': exam_item.item_code, 'result_options': selective.result_select})
+        non_selectives = net_doc.get('normal_items')
+        if non_selectives:
+          for non_selective in non_selectives:
+            doctor_doc.append('non_selective_result', {'test_name': non_selective.heading_text, 'test_event': non_selective.lab_test_event, 'test_uom': non_selective.lab_test_uom, 'min_value': non_selective.min_value, 'max_value': non_selective.max_value})
+      doctor_doc.insert()
+      return {'docname': doctor_doc.name}
+    else:
+      frappe.throw("No Template found.")
+  else:
+    frappe.throw(f"Unsupported source: {source}")
+
 def create_radiology_exam(source, name, room):
   if source == 'Dispatcher':
     appt = frappe.db.get_value('Dispatcher', name, 'patient_appointment')
