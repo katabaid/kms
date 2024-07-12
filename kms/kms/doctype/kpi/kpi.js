54f37d5a9547c03a26a555474084345a2af3df54
auliabismar <auliabismar@yahoo.com>
2024-07-12 19:35:21 +0700

    ref: moved client scripts to their doctypes

--- kms/kms/doctype/kpi/kpi.js
+++ kms/kms/doctype/kpi/kpi.js
@@ -2,7 +2,26 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('KPI', {
-	// refresh: function(frm) {
+	kpi_template: function(frm) {
+		frappe.db.get_doc('KPI Template', frm.doc.kpi_template).then(doc=>{
+		    frm.doc.kpi_details_tab =[];
+		    $.each(doc.kpi_template, (_i,e)=>{
+		        let row = frm.add_child('kpi_details_tab');
+		        row.key_result_area = e.key_result_area;
+		        row.kpi = e.kpi;
+		        row.weightage = e.weightage;
+		        row.target = e.target;
+		        row.unit_of_kpi = e.unit_of_kpi;
+		        row.higher_is_better = e.higher_is_better;
+		    });
+		    frm.refresh_field("kpi_details_tab");
+		});
+	}
+});
 
-	// }
+frappe.ui.form.on('KPI Details',  'actual', (frm, cdt, cdn) => {
+	let d = locals[cdt][cdn];
+	d.score_ = d.lower_is_better? d.target/d.actual*100 : d.actual/d.target*100;
+	d.final_score = d.lower_is_better? d.target/d.actual*d.weightage : d.actual/d.target*d.weightage;
+	frm.refresh_field("kpi_details_tab");
 });
--- kms/kms/doctype/queue_pooling/queue_pooling.js
+++ kms/kms/doctype/queue_pooling/queue_pooling.js
@@ -2,7 +2,44 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Queue Pooling', {
-	// refresh: function(frm) {
-
-	// }
+	refresh(frm) {
+		if(frm.doc.status === 'Queued'&& !frm.doc.encounter){
+			const now = new Date();
+			const hour = now.getHours();
+			const minute = now.getMinutes();
+			const second = now.getSeconds();
+			const formattedTime = hour + ":" + minute + ":" + second;
+			frm.add_custom_button(
+				__('Cancel'),
+				()=> {
+					frappe.prompt([
+						{
+							label: 'Cancel Reason',
+							fieldname: 'cancel_reason',
+							fieldtype: 'Data',
+							reqd: true
+						}], (values)=>{
+							frappe.db.set_value('Queue Pooling', frm.doc.name, {
+								status: 'Cancelled',
+								cancelled_time: formattedTime,
+								cancel_reason: values.cancel_reason
+							});
+						}
+					);
+				}
+			);
+			frm.add_custom_button(
+				__('Create Encounter'), 
+				()=>{
+					frappe.new_doc('Patient Encounter',{
+						appointment: frm.doc.appointment,
+						patient: frm.doc.patient,
+						company: frm.doc.company,
+						custom_queue_pooling: frm.doc.name,
+						custom_service_unit: frm.doc.service_unit
+					});
+				}
+			);
+		}
+	}
 });
