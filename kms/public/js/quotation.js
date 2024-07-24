e536c86e07d1c89efc89e16c8b8380bd9775e466
auliabismar <auliabismar@yahoo.com>
2024-07-24 18:58:46 +0700

    feat: new dialog box with 3 columns of dynamic selects

--- kms/public/js/quotation.js
+++ kms/public/js/quotation.js
@@ -35,67 +35,80 @@ function createSelectField(fieldname, label, options, onChange) {
   };
 }
 // Function to update the next level of select
-// Function to update the next level of select
 function updateNextLevel(dialog, currentLevel) {
-	return function() {
-		const selectedValue = dialog.get_value(`level${currentLevel}`);
-		const children = getChildren(selectedValue);
-		const nextLevel = currentLevel + 1;
-		const nextLevelField = dialog.fields_dict[`level${nextLevel}`];
-
-		if (children.length > 0 && nextLevelField) {
-			const options = [''].concat(children.map(child => child.name));
-			nextLevelField.df.options = options;
-			nextLevelField.refresh();
-			nextLevelField.$wrapper.show();
-			// Clear and hide all subsequent levels
-			for (let i = nextLevel + 1; i <= 5; i++) {
-				const field = dialog.fields_dict[`level${i}`];
-				if (field) {
-					field.set_value('');
-					field.$wrapper.hide();
-				}
-			}
-		} else {
-			// Hide all subsequent levels
-			for (let i = nextLevel; i <= 5; i++) {
-				const field = dialog.fields_dict[`level${i}`];
-				if (field) {
-					field.set_value('');
-					field.$wrapper.hide();
-				}
-			}
-		}
-	};
+  return function() {
+    const selectedValue = dialog.get_value(`level${currentLevel}`);
+    const children = getChildren(selectedValue);
+    const nextLevel = currentLevel + 1;
+    const nextLevelField = dialog.fields_dict[`level${nextLevel}`];
+    
+    if (children.length > 0 && nextLevelField) {
+      const options = [''].concat(children.map(child => child.name));
+      nextLevelField.df.options = options;
+      nextLevelField.refresh();
+      nextLevelField.$wrapper.show();
+      // Clear and hide all subsequent levels
+      for (let i = nextLevel + 1; i <= 3; i++) {
+        const field = dialog.fields_dict[`level${i}`];
+        if (field) {
+          field.set_value('');
+          field.$wrapper.hide();
+        }
+      }
+    } else {
+      // Hide all subsequent levels
+      for (let i = nextLevel; i <= 3; i++) {
+        const field = dialog.fields_dict[`level${i}`];
+        if (field) {
+          field.set_value('');
+          field.$wrapper.hide();
+        }
+      }
+    }
+  };
 }
 // Function to create the dialog box
 function createDialog() {
   const root = 'Examination';
   const firstLevelOptions = getChildren(root);
+  
   // Prepare all fields at once
   const fields = [
-    createSelectField('level1', 'Level 1', [''].concat(firstLevelOptions.map(option => option.name)))
+    {
+      fieldtype: 'Column Break'
+    },
+    createSelectField('level1', 'Level 1', [''].concat(firstLevelOptions.map(option => option.name))),
+    {
+      fieldtype: 'Column Break'
+    },
+    createSelectField('level2', 'Level 2', []),
+    {
+      fieldtype: 'Column Break'
+    },
+    createSelectField('level3', 'Level 3', [])
   ];
-  // Add placeholder fields for potential child levels
-  for (let i = 2; i <= 5; i++) {
-    fields.push(createSelectField(`level${i}`, `Level ${i}`, []));
-  }
+  
   const dialog = new frappe.ui.Dialog({
     title: 'Dynamic Select',
+    size: 'large',
     fields: fields
   });
+  
   // Set up onchange handlers for all levels
-  for (let i = 1; i <= 5; i++) {
+  for (let i = 1; i <= 3; i++) {
     dialog.fields_dict[`level${i}`].df.onchange = updateNextLevel(dialog, i);
   }
+  
   // Hide all levels except the first one
-  for (let i = 2; i <= 5; i++) {
+  for (let i = 2; i <= 3; i++) {
     dialog.fields_dict[`level${i}`].$wrapper.hide();
   }
+  
   dialog.set_primary_action(__('Submit'), function() {
     // Handle form submission here
     dialog.hide();
   });
+  
   dialog.show();
 }
 //const hideStandardChildButtons = (frm, childTable) => {null;}
@@ -105,5 +118,5 @@ const getExamItemSelection = (frm) => {
     args: {root: 'Examination'},
     freeze: true,
     callback: (r) => {json_data = r.message;}
-  })
+  });
 }
