dd2f17f0afb386003290dcdae063736ebc85c714
auliabismar <auliabismar@yahoo.com>
2024-07-31 04:34:55 +0700

    ref: doctor

--- kms/kms/doctype/doctor_examination/doctor_examination.js
+++ kms/kms/doctype/doctor_examination/doctor_examination.js
@@ -23,7 +23,6 @@ frappe.ui.form.on('Doctor Examination', {
   before_submit: function (frm) {
     // Call the custom before_submit function
     doctorExaminationController.config.before_submit(frm);
-
     // You can also add more logic here if needed
   },
 
@@ -41,43 +40,20 @@ frappe.ui.form.on('Doctor Examination', {
   refresh: function (frm) {
     // If you also want to override refresh
     doctorExaminationController.refresh(frm);
-    frm.set_df_property('eyes_section', 'hidden', 1);
-    frm.set_df_property('ear_section', 'hidden', 1);
-    frm.set_df_property('nose_section', 'hidden', 1);
-    frm.set_df_property('throat_section', 'hidden', 1);
-    frm.set_df_property('neck_section', 'hidden', 1);
-    frm.set_df_property('cardiac_section', 'hidden', 1);
-    frm.set_df_property('breast_section', 'hidden', 1);
-    frm.set_df_property('resp_section', 'hidden', 1);
-    frm.set_df_property('abd_section', 'hidden', 1);
-    frm.set_df_property('spine_section', 'hidden', 1);
-    frm.set_df_property('genit_section', 'hidden', 1);
-    frm.set_df_property('neuro_section', 'hidden', 1);
-    frm.set_df_property('visual_field_test_section', 'hidden', 1);
-    frm.set_df_property('romberg_test_section', 'hidden', 1);
-    frm.set_df_property('tinnel_test_section', 'hidden', 1);
-    frm.set_df_property('phallen_test_section', 'hidden', 1);
-    frm.set_df_property('rectal_examination_section', 'hidden', 1);
+    const sectionsToHide = [
+      'eyes_section', 'ear_section', 'nose_section', 'throat_section', 'neck_section', 
+      'cardiac_section', 'breast_section', 'resp_section', 'abd_section', 'spine_section', 
+      'genit_section', 'neuro_section', 'visual_field_test_section', 'romberg_test_section', 'tinnel_test_section',
+      'phallen_test_section', 'rectal_examination_section'
+    ];
+    sectionsToHide.forEach(section => frm.set_df_property(section, 'hidden', 1));
+
     if (frm.doc.status === 'Checked In') {
       const examItems = frm.doc.examination_item.filter(row => row.status === 'Started').map(row => row.template)
       const matchingItems = mcu_settings.filter(item => examItems.includes(item.value));
       if (matchingItems.length > 0) {
         matchingItems.forEach(item => {
-          console.log(item.field)
-          if (item.field === 'physical_examination_name') {
-            frm.set_df_property('eyes_section', 'hidden', 0);
-            frm.set_df_property('ear_section', 'hidden', 0);
-            frm.set_df_property('nose_section', 'hidden', 0);
-            frm.set_df_property('throat_section', 'hidden', 0);
-            frm.set_df_property('neck_section', 'hidden', 0);
-            frm.set_df_property('cardiac_section', 'hidden', 0);
-            frm.set_df_property('breast_section', 'hidden', 0);
-            frm.set_df_property('resp_section', 'hidden', 0);
-            frm.set_df_property('abd_section', 'hidden', 0);
-            frm.set_df_property('spine_section', 'hidden', 0);
-            frm.set_df_property('genit_section', 'hidden', 0);
-            frm.set_df_property('neuro_section', 'hidden', 0);
-          }
+          if (item.field === 'physical_examination_name') sectionsToHide.slice(0, 12).forEach(section => frm.set_df_property(section, 'hidden', 0));
           if (item.field === 'visual_field_test_name') frm.set_df_property('visual_field_test_section', 'hidden', 0);
           if (item.field === 'romberg_test_name') frm.set_df_property('romberg_test_section', 'hidden', 0);
           if (item.field === 'tinnel_test_name') frm.set_df_property('tinnel_test_section', 'hidden', 0);
