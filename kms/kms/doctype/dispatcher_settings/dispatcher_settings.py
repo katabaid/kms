e437141c4c24639e12eb8f2471e5d075beca3d3b
Aulia Bismar <auliabismar@yahoo.com>
2024-08-01 18:32:20 +0700

    fix: dispatcher setting unique keys on branch and date

--- kms/kms/doctype/dispatcher_settings/dispatcher_settings.py
+++ kms/kms/doctype/dispatcher_settings/dispatcher_settings.py
@@ -1,8 +1,15 @@
 # Copyright (c) 2024, GIS and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class DispatcherSettings(Document):
-	pass
+	def validate(self):
+		if frappe.db.exists(self.doctype,{
+			'branch': self.branch,
+			'enable_date': self.enable_date,
+			'name': ('!=', self.name)
+		}):
+			frappe.throw(_("The combination of Branch and Enable Date must be unique."))
