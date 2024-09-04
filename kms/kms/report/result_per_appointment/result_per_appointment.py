de0be9355fd528c5bc7e93e13b30342c7b618467
Aulia Bismar <auliabismar@yahoo.com>
2024-09-04 19:40:34 +0700

    fix: Result per Appointment doc exam

--- kms/kms/report/result_per_appointment/result_per_appointment.py
+++ kms/kms/report/result_per_appointment/result_per_appointment.py
@@ -76,73 +76,73 @@ def get_children(name, item, doctype):
 					IF(tde.eyes_check>0, 'No Abnormality', CONCAT_WS(', ', CASE WHEN tde.left_anemic>0 THEN 'Left Anemic' END, CASE WHEN tde.left_icteric>0 THEN 'Left Icteric' END, CASE WHEN tde.right_anemic>0 THEN 'Right Anemic' END, CASE WHEN tde.right_icteric>0 THEN 'Rightt Icteric' END)) AS result_check, 
 					CONCAT_WS(', ', CASE WHEN LENGTH(tde.eyes_left_others)>0 THEN CONCAT('Left: ', tde.eyes_left_others) END, CASE WHEN LENGTH(tde.eyes_right_others)>0 THEN CONCAT('Right: ', tde.eyes_right_others) END) AS result_text, 
 					'Eyes' AS result_line, NULL AS uom, NULL AS min_value, NULL AS max_value
-					FROM `tabDoctor Examination` tde WHERE name = 'HLC-DE-20240903-00001'
+					FROM `tabDoctor Examination` tde WHERE name = '{name}'
 					UNION
 					SELECT 
 					IF(tde.ear_check>0, 'No Abnormality', CONCAT_WS(', ', CASE WHEN tde.left_cerumen>0 THEN 'Left Cerumen' END, CASE WHEN tde.left_cerumen_prop>0 THEN 'Left Cerumen Prop' END, CASE WHEN tde.left_tympanic>0 THEN 'Left Tympanic membrane intact' END, CASE WHEN tde.right_cerumen>0 THEN 'Right Cerumen' END, CASE WHEN tde.right_cerumen_prop>0 THEN 'Right Cerumen Prop' END, CASE WHEN tde.right_tympanic>0 THEN 'Right Tympanic membrane intact' END)), 
 					CONCAT_WS(', ', CASE WHEN LENGTH(tde.ear_left_others)>0 THEN CONCAT('Left: ', tde.ear_left_others) END, CASE WHEN LENGTH(tde.ear_right_others)>0 THEN CONCAT('Right: ', tde.ear_right_others) END), 
 					'Ear', NULL, NULL, NULL
-					FROM `tabDoctor Examination` tde WHERE name = 'HLC-DE-20240903-00001'
+					FROM `tabDoctor Examination` tde WHERE name = '{name}'
 					UNION
 					SELECT 
 					IF(tde.nose_check>0, 'No Abnormality', CONCAT_WS(', ', CASE WHEN tde.deviated>0 THEN 'Deviated' END, CASE WHEN tde.left_enlarged>0 THEN 'Left Enlarged' END, CASE WHEN tde.left_hyperemic>0 THEN 'Left Hyperemic' END, CASE WHEN tde.left_polyp>0 THEN 'Left Polyp' END, CASE WHEN tde.right_enlarged>0 THEN 'Right Enlarged' END, CASE WHEN tde.right_hyperemic>0 THEN 'Right Hyperemic' END, CASE WHEN tde.right_polyp>0 THEN 'Right Polyp' END)), 
 					CONCAT_WS(', ', CASE WHEN LENGTH(tde.nose_left_others)>0 THEN CONCAT('Left: ', tde.nose_left_others) END, CASE WHEN LENGTH(tde.nose_right_others)>0 THEN CONCAT('Right: ', tde.nose_right_others) END), 
 					'Nose', NULL, NULL, NULL
-					FROM `tabDoctor Examination` tde WHERE name = 'HLC-DE-20240903-00001'
+					FROM `tabDoctor Examination` tde WHERE name = '{name}'
 					UNION
 					SELECT 
 					IF(tde.throat_check>0, 'No Abnormality', CONCAT_WS(', ', CASE WHEN tde.enlarged_tonsil>0 THEN 'Enlarged Tonsil' END, CASE WHEN tde.hyperemic_pharynx>0 THEN 'Hyperemic Pharynx' END)),
 					tde.throat_others, 
 					'Throat', NULL, NULL, NULL
-					FROM `tabDoctor Examination` tde WHERE name = 'HLC-DE-20240903-00001'
+					FROM `tabDoctor Examination` tde WHERE name = '{name}'
 					UNION
 					SELECT 
 					IF(tde.neck_check>0, 'No Abnormality', CONCAT_WS(', ', CASE WHEN tde.enlarged_thyroid>0 THEN 'Enlarged Thyroid' END, CASE WHEN tde.enlarged_lymph_node>0 THEN 'Enlarged Lymph Node' END)),
 					tde.neck_others, 
 					'Neck', NULL, NULL, NULL
-					FROM `tabDoctor Examination` tde WHERE name = 'HLC-DE-20240903-00001'
+					FROM `tabDoctor Examination` tde WHERE name = '{name}'
 					UNION
 					SELECT 
 					IF(tde.cardiac_check>0, 'No Abnormality', CONCAT_WS(', ', CASE WHEN tde.regular_heart_sound>0 THEN 'Regular Heart Sound' END, CASE WHEN tde.murmur>0 THEN 'Murmur' END, CASE WHEN tde.gallop>0 THEN 'Gallop' END)),
 					tde.others, 
 					'Cardiac', NULL, NULL, NULL
-					FROM `tabDoctor Examination` tde WHERE name = 'HLC-DE-20240903-00001'
+					FROM `tabDoctor Examination` tde WHERE name = '{name}'
 					UNION
 					SELECT 
 					IF(tde.breast_check>0, 'No Abnormality', CONCAT_WS(', ', CASE WHEN tde.left_enlarged_breast>0 THEN 'Enlarged Left Breast Glands' END, CASE WHEN tde.left_lumps>0 THEN 'Left Lumps' END, CASE WHEN tde.right_enlarged_breast>0 THEN 'Enlarged Right Breast Glands' END, CASE WHEN tde.right_lumps>0 THEN 'Right Lumps' END)), 
 					CONCAT_WS(', ', CASE WHEN LENGTH(tde.breast_left_others)>0 THEN CONCAT('Left: ', tde.breast_left_others) END, CASE WHEN LENGTH(tde.breast_right_others)>0 THEN CONCAT('Right: ', tde.breast_right_others) END), 
 					'Breast', NULL, NULL, NULL
-					FROM `tabDoctor Examination` tde WHERE name = 'HLC-DE-20240903-00001'
+					FROM `tabDoctor Examination` tde WHERE name = '{name}'
 					UNION
 					SELECT 
 					IF(tde.resp_check>0, 'No Abnormality', CONCAT_WS(', ', CASE WHEN tde.left_ronkhi>0 THEN 'Left Ronkhi' END, CASE WHEN tde.left_wheezing>0 THEN 'Left Wheezing' END, CASE WHEN tde.right_ronkhi>0 THEN 'Enlarged Ronkhi' END, CASE WHEN tde.right_wheezing>0 THEN 'Right Wheezing' END)), 
 					CONCAT_WS(', ', CASE WHEN LENGTH(tde.resp_left_others)>0 THEN CONCAT('Left: ', tde.resp_left_others) END, CASE WHEN LENGTH(tde.resp_right_others)>0 THEN CONCAT('Right: ', tde.resp_right_others) END), 
 					'Respiratory System', NULL, NULL, NULL
-					FROM `tabDoctor Examination` tde WHERE name = 'HLC-DE-20240903-00001'
+					FROM `tabDoctor Examination` tde WHERE name = '{name}'
 					UNION
 					SELECT 
 					IF(tde.abd_check>0, 'No Abnormality', CONCAT_WS(', ', CASE WHEN tde.tenderness>0 THEN 'Tenderness' END, CASE WHEN tde.hepatomegaly>0 THEN 'Hepatomegaly' END, CASE WHEN tde.splenomegaly>0 THEN 'Splenomegaly' END, CASE WHEN tde.increased_bowel_sounds>0 THEN 'Increased Bowel Sounds' END)),
 					tde.abd_others, 
 					'Abdomen', NULL, NULL, NULL
-					FROM `tabDoctor Examination` tde WHERE name = 'HLC-DE-20240903-00001'
+					FROM `tabDoctor Examination` tde WHERE name = '{name}'
 					UNION
 					SELECT 
 					IF(tde.spine_check>0, 'No Abnormality', ''),
 					tde.spine_details, 
 					'Spine', NULL, NULL, NULL
-					FROM `tabDoctor Examination` tde WHERE name = 'HLC-DE-20240903-00001'
+					FROM `tabDoctor Examination` tde WHERE name = '{name}'
 					UNION
 					SELECT 
 					IF(tde.genit_check>0, 'No Abnormality', CONCAT_WS(', ', CASE WHEN tde.hernia>0 THEN 'Hernia' END, CASE WHEN tde.hemorrhoid>0 THEN 'Hemorrhoid' END, CASE WHEN tde.inguinal_nodes>0 THEN 'Inguinal Nodes' END)),
 					tde.genit_others, 
 					'Genitourinary', NULL, NULL, NULL
-					FROM `tabDoctor Examination` tde WHERE name = 'HLC-DE-20240903-00001'
+					FROM `tabDoctor Examination` tde WHERE name = '{name}'
 					UNION
 					SELECT 
 					IF(tde.neuro_check>0, 'No Abnormality', CONCAT_WS(', ', CASE WHEN tde.motoric_system_abnormality>0 THEN 'Motoric System Abnormality' END, CASE WHEN tde.reflexes_abnormality>0 THEN 'Reflexes Abnormality' END, CASE WHEN tde.sensory_system_abnormality>0 THEN 'Sensory System Abnormality' END)),
 					tde.neuro_others, 
 					'Neurological System', NULL, NULL, NULL
-					FROM `tabDoctor Examination` tde WHERE name = 'HLC-DE-20240903-00001'
+					FROM `tabDoctor Examination` tde WHERE name = '{name}'
 					""", as_dict=1)
 			elif matching_field == 'visual_field_test':
 				return frappe.db.sql(f"""SELECT 
