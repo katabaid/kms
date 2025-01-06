0bfe1a0b20e7cce43ef643fd85f1d3c75bf5a059
Aulia Bismar <auliabismar@yahoo.com>
2025-01-06 07:37:07 +0700

    feat: set correct dispatcher on  dispatcher settings according to their role and user permission

--- kms/kms/doctype/dispatcher_settings/dispatcher_settings.js
+++ kms/kms/doctype/dispatcher_settings/dispatcher_settings.js
@@ -4,5 +4,18 @@
 frappe.ui.form.on('Dispatcher Settings', {
 	onload: function(frm) {
 		frappe.breadcrumbs.add('Healthcare', 'Dispatcher Settings');
-	}
+	},
+	refresh: function(frm){
+		frm.set_query('dispatcher', function() {
+			return {
+				query: 'kms.kms.doctype.dispatcher_settings.dispatcher_settings.get_dispatchers_for_branch',
+				filters: {
+					'branch': frm.doc.branch
+				}
+			}
+		})
+	},
+	branch: function(frm) {
+		frm.set_value('dispatcher', '')
+	},
 });
--- kms/kms/doctype/dispatcher_settings/dispatcher_settings.py
+++ kms/kms/doctype/dispatcher_settings/dispatcher_settings.py
@@ -12,3 +12,42 @@ class DispatcherSettings(Document):
 			'enable_date': self.enable_date,
 		}):
 			frappe.throw(_("The combination of Branch and Enable Date must be unique."))
+
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def get_dispatchers_for_branch(doctype, txt, searchfield, start, page_len, filters):
+	branch = filters.get('branch')
+	if not branch:
+			return []
+			
+	# Get users with HC Dispatcher role who either have this branch
+	# or don't have any branch permissions
+	return frappe.db.sql("""
+		SELECT DISTINCT u.name, u.full_name 
+		FROM `tabUser` u
+		INNER JOIN `tabHas Role` hr ON hr.parent = u.name
+		LEFT JOIN `tabUser Permission` up ON up.user = u.name
+		WHERE hr.role = 'HC Dispatcher'
+		AND hr.parenttype = 'User'
+		AND u.enabled = 1
+		AND (
+			u.name IN (
+				SELECT user
+				FROM `tabUser Permission`
+				WHERE allow = 'Branch' 
+				AND for_value = %(branch)s
+			)
+			OR u.name NOT IN (
+				SELECT user
+				FROM `tabUser Permission`
+				WHERE allow = 'Branch'
+			)
+		)
+		AND (u.name LIKE %(txt)s OR u.full_name LIKE %(txt)s)
+		ORDER BY u.full_name
+		LIMIT %(start)s, %(page_len)s""", {
+		'branch': branch,
+		'txt': "%%%s%%" % txt,
+		'start': start,
+		'page_len': page_len
+	})
