7ab79c909bc7f30040d58906e87a1275911d6ff3
Aulia Bismar <auliabismar@yahoo.com>
2025-04-14 11:24:29 +0700

    feat: new doc FO Payment

--- kms/public/js/sales_invoice.js
+++ kms/public/js/sales_invoice.js
@@ -1,13 +1,18 @@
 frappe.ui.form.on('Sales Invoice', {
 	refresh(frm) {
-		if (frm.doc.docstatus === 0 && !frm.doc.is_return) {
+		if (frm.doc.docstatus === 0 && !frm.doc.is_return && !frm.is_new()) {
 			frm.add_custom_button(__('Healthcare'), function() {
 				get_healthcare_to_invoice(frm);
-			},__('Get Items From'));
+			},__('Front Office'));
 			frm.add_custom_button(__('MCU'), function() {
 				get_mcu_to_invoice(frm);
-			},__('Get Items From'));
+			},__('Front Office'));
 		}
+    if (frm.doc.docstatus == 1 && !frm.doc.is_return){
+      frm.add_custom_button(__('Payment'), function() {
+        create_fo_payment(frm);
+      },__('Front Office'));
+    }
     frm.remove_custom_button('Healthcare Services', 'Get Items From');
     frm.remove_custom_button('Prescriptions', 'Get Items From');
 	},
@@ -204,4 +209,29 @@ const get_mcu_to_invoice = (frm) => {
     }
   });
   return d;
+}
+
+const create_fo_payment = async (frm) => {
+  const branch = await frappe.db.get_value('Patient Appointment', frm.doc.custom_exam_id, 'custom_branch');
+  const customer = await frappe.db.get_value('Customer', frm.doc.customer, 'customer_name');
+  console.log(branch.message.custom_branch)
+  console.log(customer.message.customer_name)
+  frappe.new_doc('FO Payment', {
+    company: frm.doc.company,
+    invoice: frm.doc.name,
+    branch: branch.message.custom_branch||'',
+    posting_date: new Date(),
+    cost_center: frm.doc.cost_center,
+    patient: frm.doc.patient,
+    customer: frm.doc.customer,
+    outstanding_amount: frm.doc.outstanding_amount,
+    cost_center: frm.doc.cost_center,
+    patient_name: frm.doc.patient_name,
+    customer_name: customer.message.customer_name||'',
+    items: [{
+      mode_of_payment: 'Cash',
+      amount: frm.doc.outstanding_amount,
+    }],
+    total_payment: frm.doc.outstanding_amount,
+  })
 }
