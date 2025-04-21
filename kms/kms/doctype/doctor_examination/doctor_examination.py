19b1e461d63a740d018d88a4f6e94c28f5de01c0
Aulia Bismar <auliabismar@yahoo.com>
2025-04-21 22:32:44 +0700

    ref: doc exam for dental

--- kms/kms/doctype/doctor_examination/doctor_examination.py
+++ kms/kms/doctype/doctor_examination/doctor_examination.py
@@ -8,67 +8,23 @@ class DoctorExamination(Document):
 	def before_insert(self):
 		if not self.is_dental_record_inserted:
 			item_name = frappe.db.get_single_value ('MCU Settings', 'dental_examination_name')
-			for exam_item in self.examination_item:
-				if exam_item.template == item_name:
-					prepared_data = [
-						{'teeth_type': 'Permanent Teeth', 'location': 'ul', 'position': 18},
-						{'teeth_type': 'Permanent Teeth', 'location': 'ul', 'position': 17},
-						{'teeth_type': 'Permanent Teeth', 'location': 'ul', 'position': 16},
-						{'teeth_type': 'Permanent Teeth', 'location': 'ul', 'position': 15},
-						{'teeth_type': 'Permanent Teeth', 'location': 'ul', 'position': 14},
-						{'teeth_type': 'Permanent Teeth', 'location': 'ul', 'position': 13},
-						{'teeth_type': 'Permanent Teeth', 'location': 'ul', 'position': 12},
-						{'teeth_type': 'Permanent Teeth', 'location': 'ul', 'position': 11},
-						{'teeth_type': 'Permanent Teeth', 'position': 21, 'location': 'ur'},
-						{'teeth_type': 'Permanent Teeth', 'position': 22, 'location': 'ur'},
-						{'teeth_type': 'Permanent Teeth', 'position': 23, 'location': 'ur'},
-						{'teeth_type': 'Permanent Teeth', 'position': 24, 'location': 'ur'},
-						{'teeth_type': 'Permanent Teeth', 'position': 25, 'location': 'ur'},
-						{'teeth_type': 'Permanent Teeth', 'position': 26, 'location': 'ur'},
-						{'teeth_type': 'Permanent Teeth', 'position': 27, 'location': 'ur'},
-						{'teeth_type': 'Permanent Teeth', 'position': 28, 'location': 'ur'},
-						{'teeth_type': 'Permanent Teeth', 'position': 48, 'location': 'll'},
-						{'teeth_type': 'Permanent Teeth', 'position': 47, 'location': 'll'},
-						{'teeth_type': 'Permanent Teeth', 'position': 46, 'location': 'll'},
-						{'teeth_type': 'Permanent Teeth', 'position': 45, 'location': 'll'},
-						{'teeth_type': 'Permanent Teeth', 'position': 44, 'location': 'll'},
-						{'teeth_type': 'Permanent Teeth', 'position': 43, 'location': 'll'},
-						{'teeth_type': 'Permanent Teeth', 'position': 42, 'location': 'll'},
-						{'teeth_type': 'Permanent Teeth', 'position': 41, 'location': 'll'},
-						{'teeth_type': 'Permanent Teeth', 'position': 31, 'location': 'lr'},
-						{'teeth_type': 'Permanent Teeth', 'position': 32, 'location': 'lr'},
-						{'teeth_type': 'Permanent Teeth', 'position': 33, 'location': 'lr'},
-						{'teeth_type': 'Permanent Teeth', 'position': 34, 'location': 'lr'},
-						{'teeth_type': 'Permanent Teeth', 'position': 35, 'location': 'lr'},
-						{'teeth_type': 'Permanent Teeth', 'position': 36, 'location': 'lr'},
-						{'teeth_type': 'Permanent Teeth', 'position': 37, 'location': 'lr'},
-						{'teeth_type': 'Permanent Teeth', 'position': 38, 'location': 'lr'},
-						{'teeth_type': 'Primary Teeth', 'position': 55, 'location': 'ul'},
-						{'teeth_type': 'Primary Teeth', 'position': 54, 'location': 'ul'},
-						{'teeth_type': 'Primary Teeth', 'position': 53, 'location': 'ul'},
-						{'teeth_type': 'Primary Teeth', 'position': 52, 'location': 'ul'},
-						{'teeth_type': 'Primary Teeth', 'position': 51, 'location': 'ul'},
-						{'teeth_type': 'Primary Teeth', 'position': 61, 'location': 'ur'},
-						{'teeth_type': 'Primary Teeth', 'position': 62, 'location': 'ur'},
-						{'teeth_type': 'Primary Teeth', 'position': 63, 'location': 'ur'},
-						{'teeth_type': 'Primary Teeth', 'position': 64, 'location': 'ur'},
-						{'teeth_type': 'Primary Teeth', 'position': 65, 'location': 'ur'},
-						{'teeth_type': 'Primary Teeth', 'position': 85, 'location': 'll'},
-						{'teeth_type': 'Primary Teeth', 'position': 84, 'location': 'll'},
-						{'teeth_type': 'Primary Teeth', 'position': 83, 'location': 'll'},
-						{'teeth_type': 'Primary Teeth', 'position': 82, 'location': 'll'},
-						{'teeth_type': 'Primary Teeth', 'position': 81, 'location': 'll'},
-						{'teeth_type': 'Primary Teeth', 'position': 71, 'location': 'lr'},
-						{'teeth_type': 'Primary Teeth', 'position': 72, 'location': 'lr'},
-						{'teeth_type': 'Primary Teeth', 'position': 73, 'location': 'lr'},
-						{'teeth_type': 'Primary Teeth', 'position': 74, 'location': 'lr'},
-						{'teeth_type': 'Primary Teeth', 'position': 75, 'location': 'lr'},
-					]
-					for data in prepared_data:
+			if any(exam_item.template == item_name for exam_item in self.examination_item):
+				teeth_map = [
+					{"type": "Permanent Teeth", "loc": "ul", "range": range(18, 10, -1)},
+					{"type": "Permanent Teeth", "loc": "ur", "range": range(21, 29)},
+					{"type": "Permanent Teeth", "loc": "ll", "range": range(48, 40, -1)},
+					{"type": "Permanent Teeth", "loc": "lr", "range": range(31, 39)},
+					{"type": "Primary Teeth", "loc": "ul", "range": range(55, 50, -1)},
+					{"type": "Primary Teeth", "loc": "ur", "range": range(61, 66)},
+					{"type": "Primary Teeth", "loc": "ll", "range": range(85, 80, -1)},
+					{"type": "Primary Teeth", "loc": "lr", "range": range(71, 76)}
+				]
+				for section in teeth_map:				
+					for pos in section['range']:
 						self.append('dental_detail', {
-							'teeth_type': data['teeth_type'],
-							'position': data['position'],
-							'location': data['location']
+							'teeth_type': section['type'],
+							'position': pos,
+							'location': section['loc']
 						})
 		pa_doc = frappe.get_doc('Patient Appointment', self.appointment)
 		for package in pa_doc.custom_mcu_exam_items:
@@ -102,15 +58,12 @@ class DoctorExamination(Document):
 					elif doctor_results[0].docstatus==0:
 						dental_item = frappe.db.get_single_value('MCU Settings', 'dental_examination')
 						doctor_result = frappe.get_doc("Doctor Result", doctor_results[0].name)
-						found = False
 						for row in doctor_result.doctor_grade:
 							if row.hidden_item == dental_item:
 								row.grade = self.grade
-								found = True
+								doctor_result.save(ignore_permissions=True)
+								frappe.msgprint(f"Updated dental grade in Doctor Result {doctor_result.name}")
 								break
-						if found:
-							doctor_result.save(ignore_permissions=True)
-							frappe.msgprint(f"Updated dental grade in Doctor Result {doctor_result.name}")
 
 def setup_questionnaire_table(self, item):
 	is_internal = frappe.db.get_value('Questionnaire Template', item.item_name, 'internal_questionnaire')
