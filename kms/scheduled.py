813fb0642c89b4198729a3aaed603d24e3e83adf
Aulia Bismar <auliabismar@yahoo.com>
2025-05-26 16:59:08 +0700

    ref: scheduled.py

--- kms/scheduled.py
+++ kms/scheduled.py
@@ -1,52 +1,45 @@
 import frappe
-from frappe.utils import now_datetime, get_datetime
+from frappe.utils import getdate, today, now_datetime, get_datetime
 from datetime import timedelta
 
 def set_cancelled_open_appointment():
-  appointment = frappe.db.get_list('Patient Appointment', filters={'status': 'Open'}, fields=['name', 'appointment_date'])
-  today = frappe.utils.getdate(frappe.utils.today())
+  appointment = frappe.db.get_list(
+    'Patient Appointment', filters={'status': 'Open'}, fields=['name', 'appointment_date'])
   for q in appointment:
-    if q['appointment_date'] < today:
-      doc = frappe.get_doc('Patient Appointment', q['name'])
-      doc.status = 'Cancelled'
-      doc.save()
+    if q['appointment_date'] < getdate(today()):
+      frappe.db.set_value('Patient Appointment', q['name'], 'status', 'Cancelled')
 
 def set_open_scheduled_appointment():
-  appointment = frappe.db.get_list('Patient Appointment', filters={'status': 'Scheduled'}, fields=['name', 'appointment_date'])
-  today = frappe.utils.getdate(frappe.utils.today())
+  appointment = frappe.db.get_list(
+    'Patient Appointment', filters={'status': 'Scheduled'}, fields=['name', 'appointment_date'])
   for q in appointment:
-    if q['appointment_date'] == today:
-      doc = frappe.get_doc('Patient Appointment', q['name'])
-      doc.status = 'Open'
-      doc.save()
+    if q['appointment_date'] == getdate(today()):
+      frappe.db.set_value('Patient Appointment', q['name'], 'status', 'Open')
 
 def set_no_show_queue_pooling():
-  queue_pooling = frappe.db.get_list('Queue Pooling', filters={'status': 'Scheduled'}, fields=['name', 'date'])
+  queue_pooling = frappe.db.get_list(
+    'Queue Pooling', filters={'status': 'Scheduled'}, fields=['name', 'date'])
   for q in queue_pooling:
-    if q['date']<frappe.utils.today():
-      doc = frappe.get_doc('Queue Pooling', q['name'])
-      doc.status = 'No Show'
-      doc.save()
+    if q['date'] < getdate(today()):
+      frappe.db.set_value('Queue Pooling', q['name'], 'status', 'No Show')
 
 def set_cancelled_timeout_queue_pooling():
-  queue_pooling = frappe.db.get_list('Queue Pooling', filters={'status': 'Queued'}, fields=['name', 'date'])
+  queue_pooling = frappe.db.get_list(
+    'Queue Pooling', filters={'status': 'Queued'}, fields=['name', 'date'])
   for q in queue_pooling:
-    if q['date']!=frappe.utils.today():
-      doc = frappe.get_doc('Queue Pooling', q['name'])
-      doc.status = 'Cancelled'
-      doc.cancel_reason = 'Timeout'
-      doc.save()
+    if q['date'] != getdate(today()):
+      frappe.db.set_value('Queue Pooling', q['name'], {
+        'status': 'Cancelled', 'cancel_reason': 'Timeout'
+      })
 
 def reset_room_assignment():
   room_assignment = frappe.db.get_list('Room Assignment', filters={'assigned': 1}, pluck='name')
   for ra in room_assignment:
     frappe.db.set_value('Room Assignment', ra, 'assigned', 0)
-  name = frappe.db.get_all('User Permission', 
-    filters = {'allow', '=', 'Healthcare Service Unit'},
-    pluck = 'name')
+  name = frappe.db.get_all('User Permission', pluck = 'name',
+    filters = {'allow', '=', 'Healthcare Service Unit'})
   for up in name:
     frappe.db.delete('User Permission', {'name': up})
-
 
 def reset_meal_status():
   interval = frappe.db.get_single_value('MCU Settings', 'meal_time')
@@ -66,10 +59,9 @@ def reset_meal_status():
 def reset_qp_in_room():
   try:
     affected_rows = frappe.db.sql("""
-      UPDATE `tabMCU Queue Pooling`
-      SET in_room = 0, delay_time = NULL
+      UPDATE `tabMCU Queue Pooling` SET in_room = 0, delay_time = NULL
       WHERE delay_time IS NOT NULL AND delay_time <= %s
-    """, (frappe.utils.now_datetime(),))
+    """, (now_datetime(),))
     frappe.db.commit()
     frappe.log(f"Reset {affected_rows} rows in MCU Queue Pooling")
   except Exception as e:
