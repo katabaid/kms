a9847c3c987c7163fe5ecc2a7da7b65e4bcaf68c
Aulia Bismar <auliabismar@yahoo.com>
2025-06-20 16:16:57 +0700

    fix: MCU Queue Pooling Meal button

---
+++ kms/api/healthcare.py
@@ -0,0 +1,8 @@
+import frappe
+from frappe.utils import now
+
+def set_mqp_meal_time(exam_id):
+  mqps = frappe.db.get_all(
+    'MCU Queue Pooling', filters={'patient_appointment': exam_id}, pluck='name')
+  for mqp in mqps:
+    frappe.db.set_value('MCU Queue Pooling', mqp, {'meal_time': now(), 'meal_time_block': 1})
--- kms/kms/doctype/mcu_queue_pooling/mcu_queue_pooling.js
+++ kms/kms/doctype/mcu_queue_pooling/mcu_queue_pooling.js
@@ -3,10 +3,12 @@
 
 frappe.ui.form.on("MCU Queue Pooling", {
 	refresh(frm) {
-    if(frm.doc.is_meal_time){
+    const meal_status = 
+      ['Wait for Room Assignment', 'Additional or Retest Request', 'Wait for Sample']
+    if(frm.doc.is_meal_time && frm.doc.status in meal_status){
       frm.add_custom_button('Set Meal Time', () =>{
-        frm.set_value('meal_time', frappe.datetime.now_datetime())
-        frm.set_value('meal_time_block', 1)
+        frappe.call(
+          'kms.api.healthcare.set_mqp_meal_time', {exam_id: frm.doc.patient_appointment})
       })
     }
 	},
